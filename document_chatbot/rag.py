ac7c8253f4f088d202fecd4f86fefb97768ca175
cintiaching <cintiaching@lcjgroup.com>
2023-11-25 17:17:02 +0800

    move qa_chain func to rag

--- document_chatbot/rag.py
+++ document_chatbot/rag.py
@@ -1,7 +1,3 @@
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.llms import LlamaCpp
-
 from langchain.document_loaders import UnstructuredWordDocumentLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
@@ -11,26 +7,6 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 
 from langchain.document_loaders import PyPDFLoader
-
-from document_chatbot.utils import LLAMA2_13B_MODEL_PATH
-
-
-def init_llama2_13b_llm(n_gpu_layers=1000, *kwarg):
-    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-    # if the model is not stored locally, download from
-    # https://llama-cpp-python.readthedocs.io/en/latest/api-reference/#llama_cpp.Llama
-    llm = LlamaCpp(
-        model_path=LLAMA2_13B_MODEL_PATH,
-        n_gpu_layers=n_gpu_layers,  # Number of layers to offload to GPU (-ngl). If -1, all layers are offloaded.
-        n_ctx=2048,  # Context size, text limits for responses
-        # f16_kv MUST set to True, otherwise you will run into problem after a couple of calls
-        # Use half-precision for key/value cache.
-        f16_kv=True,
-        callback_manager=callback_manager,
-        verbose=False,
-        *kwarg
-    )
-    return llm
 
 
 def init_qa_chain(path, llm, chunk_size=500, chunk_overlap=20, prompt=None, doc_type="docx"):
