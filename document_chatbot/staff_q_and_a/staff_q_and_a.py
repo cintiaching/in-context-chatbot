8015747cce42450d5e9a9597b7c7a5ab60c92c7f
cintiaching <cintiaching@lcjgroup.com>
2023-12-03 13:32:31 +0800

    add StaffQAChatbot and demo notebook

---
+++ document_chatbot/staff_q_and_a/staff_q_and_a.py
@@ -0,0 +1,42 @@
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+from document_chatbot.rag import DocumentChatbot
+
+
+class StaffQAChatbot(DocumentChatbot):
+    def get_splits(self):
+        document = self.docs[0].page_content
+        result = {}
+        question = None
+        for text in document.split("\n"):
+            if text == "":
+                continue
+            if "?" in text:
+                question = text
+                result[question] = []
+                continue
+            if question is not None:
+                result[question].append(text)
+
+        answers = []
+        metadatas = []
+        for k, v in result.items():
+            answers.append("\n".join(v))
+            metadatas.append({"question": k})
+        return answers, metadatas
+
+    def get_vectorstore(self):
+        embeddings_model_name = "sentence-transformers/all-MiniLM-L6-v2"
+        embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+
+        vectorstore = Chroma(
+            collection_name="q_and_a",
+            embedding_function=embeddings,
+        )
+        answer, metadatas = self.splits
+        vectorstore.add_texts(texts=answer, metadatas=metadatas)
+        return vectorstore
+
+    def get_retriever(self):
+        retriever = self.vectorstore.as_retriever()
+        return retriever
