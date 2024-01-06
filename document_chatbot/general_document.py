08ca11b8bdb9010d6a5ee799ec31ffaede830a72
cintiaching <cintiaching@lcjgroup.com>
2024-01-06 13:51:31 +0800

    add general document rag

---
+++ document_chatbot/general_document.py
@@ -0,0 +1,27 @@
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+from document_chatbot.rag import DocumentChatbot
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+class GeneralChatbot(DocumentChatbot):
+    def __init__(self, doc_path: str, model_name: str, chunk_size=2000, chunk_overlap=100):
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+        super().__init__(model_name, doc_path)
+
+    def get_splits(self):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap)
+        all_splits = text_splitter.split_documents(self.docs)
+        return all_splits
+
+    def get_vectorstore(self):
+        # using the fastest embedding model for demo
+        embeddings_model_name = "sentence-transformers/all-MiniLM-L6-v2"
+        embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+        vectorstore = Chroma.from_documents(self.splits, embeddings)
+        return vectorstore
+
+    def get_retriever(self):
+        retriever = self.vectorstore.as_retriever()
+        return retriever
