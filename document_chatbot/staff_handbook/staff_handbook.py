43057cfe56adc005c13f8d28b2be3ad37ea776d4
cintiaching <cintiaching@lcjgroup.com>
2024-01-13 21:52:34 +0800

    bugfix

--- document_chatbot/staff_handbook/staff_handbook.py
+++ document_chatbot/staff_handbook/staff_handbook.py
@@ -1,26 +1,26 @@
 from langchain.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
-from document_chatbot.rag import DocumentChatbot
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from chromadb.errors import InvalidDimensionException
+
+from document_chatbot.rag import DocumentChatbot
 
 
 class StaffHandbookChatbot(DocumentChatbot):
     def get_splits(self):
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=20)
         all_splits = text_splitter.split_documents(self.docs)
-        metadatas = None
-        return all_splits, metadatas
+        return all_splits
 
     def get_vectorstore(self):
-        embeddings_model_name = "sentence-transformers/all-MiniLM-L6-v2"    # To be changed
+        # using the fastest embedding model for demo
+        embeddings_model_name = "sentence-transformers/all-MiniLM-L6-v2"
         embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-
-        vectorstore = Chroma(
-            collection_name="staff_handbook",
-            embedding_function=embeddings,
-        )
-        answer, metadatas = self.splits
-        vectorstore.add_texts(texts=answer, metadatas=metadatas)
+        try:
+            vectorstore = Chroma.from_documents(self.splits, embeddings)
+        except InvalidDimensionException:
+            Chroma().delete_collection()
+            vectorstore = Chroma.from_documents(self.splits, embeddings)
         return vectorstore
 
     def get_retriever(self):
