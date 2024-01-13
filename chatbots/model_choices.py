7165711ca78c59e8fa2a6681845a07315679253a
Cintia Ching <87516929+cintiaching@users.noreply.github.com>
2024-01-13 22:28:47 +0800

    tidy up llm model choices (#10)
    * rename document chatbot to chatbots
    
    * put all llms in model_choices.py
    
    * use LLMFactory in rag
    
    * delete old llm files

---
+++ chatbots/model_choices.py
@@ -0,0 +1,78 @@
+import os
+from enum import Enum
+from dotenv import load_dotenv
+
+from langchain.llms import LlamaCpp
+from langchain.chat_models import AzureChatOpenAI
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+from chatbots.utils import LLAMA2_13B_MODEL_PATH
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+class LLMs(Enum):
+    LLAMA2_13B = "llama-2-13b-chat.Q5_K_M"
+    GPT_3_PT_5_TURBO = "gpt-3.5-turbo"
+
+
+class LLMConfig:
+    def __init__(self, model_name, model_path=None, **kwarg):
+        self.model_name = model_name
+        self.model_path = model_path
+        self.model_kwarg = kwarg
+
+
+class LLMFactory:
+    @staticmethod
+    def initiate_llm(config: LLMConfig):
+        callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+        # Add more cases if needed for different llms
+        if config.model_name == LLMs.LLAMA2_13B:
+            if config.model_path is None:
+                config.model_path = LLAMA2_13B_MODEL_PATH
+            if config.model_kwarg is None:
+                # use default config
+                config.model_kwarg = {
+                    "n_gpu_layers": 1000,  # Number of layers to offload to GPU (-ngl). If -1, all layers are offloaded.
+                    "n_ctx": 2048,  # Context size, text limits for responses
+                    "verbose": False,
+                }
+            return LlamaCpp(
+                model_path=config.model_path,
+                f16_kv=True,  # f16_kv MUST set to True, issue with llamacpp
+                callback_manager=callback_manager,
+                **config.model_kwarg,
+            )
+        elif config.model_name == LLMs.GPT_3_PT_5_TURBO:
+            if config.model_kwarg is None:
+                # use default config
+                config.model_kwarg = {
+                    "temperature": 0,
+                    "max_tokens": 256,
+                }
+            return AzureChatOpenAI(
+                model_name=config.model_name.value,
+                callback_manager=callback_manager,
+                deployment_name=os.environ.get("AZURE_OPENAI_DEPLOYMENT_NAME", None),
+                **config.model_kwarg,
+            )
+        else:
+            ValueError(f"Invalid LLM config with model name '{config.model_name}'")
+
+
+class TokenCounter:
+    def __init__(self):
+        self._count = 0
+
+    def __add__(self, other):
+        self._count += other
+        return self
+
+    def add(self, other):
+        self._count += other
+
+    def total_token_used(self):
+        return self._count
--- chatbots/rag.py
+++ chatbots/rag.py
@@ -1,16 +1,15 @@
 from abc import ABC, abstractmethod
-from document_chatbot.llama2_model import init_llama2_13b_llm
-from document_chatbot.openai_model import init_openai_model
 from langchain.document_loaders import UnstructuredWordDocumentLoader
 
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 
 from langchain.document_loaders import PyPDFium2Loader
+from chatbots.model_choices import LLMs, LLMConfig, LLMFactory
 
 
 class DocumentChatbot(ABC):
-    def __init__(self, doc_path: str, prompt_msg: str = None, model_name: str = "llama2_13b"):
+    def __init__(self, model_name: LLMs, doc_path: str, prompt_msg: str = None):
         self.model_name = model_name
         self.doc_path = doc_path
         self.prompt_msg = prompt_msg
@@ -22,13 +21,8 @@ class DocumentChatbot(ABC):
         self.retriever = self.get_retriever()
 
     def get_llm(self):
-        if self.model_name == "llama2_13b":
-            llm = init_llama2_13b_llm()
-        elif self.model_name == "openai":
-            llm = init_openai_model()
-        else:
-            raise ValueError(f"Model {self.model_name} is not supported.")
-        return llm
+        config = LLMConfig(model_name=self.model_name)
+        return LLMFactory.initiate_llm(config)
 
     def load_document(self):
         file_extension = self.doc_path.split(".")[-1]
@@ -44,8 +38,11 @@ class DocumentChatbot(ABC):
         return docs
 
     def get_prompt(self):
-        if self.prompt_msg is None:
-            if self.model_name == "llama2_13b":
+        if self.prompt_msg:
+            prompt = PromptTemplate.from_template(self.prompt_msg)
+        else:
+            # use default prompt
+            if self.model_name == LLMs.LLAMA2_13B:
                 prompt = PromptTemplate.from_template(
                     "You are an assistant for question-answering tasks. "
                     "Use the following pieces of retrieved context to answer the question. "
@@ -53,7 +50,7 @@ class DocumentChatbot(ABC):
                     "try to use exact wording from context that is relevant, Keep the answer concise but give details"
                     "<</SYS>> \nQuestion: {question} \nContext: {context} \nAnswer: [/INST]"
                 )
-            elif self.model_name == "openai":
+            elif self.model_name == LLMs.GPT_3_PT_5_TURBO:
                 prompt = PromptTemplate.from_template(
                     "You are an assistant for question-answering tasks. "
                     "Use the following pieces of retrieved context to answer the question. "
@@ -61,8 +58,14 @@ class DocumentChatbot(ABC):
                     "Plz use exact wording from context that is relevant, and give details"
                     "\nQuestion: {question} \nContext: {context} "
                 )
-        else:
-            prompt = PromptTemplate.from_template(self.prompt_msg)
+            else:
+                prompt = PromptTemplate.from_template(
+                    "You are an assistant for question-answering tasks. "
+                    "Use the following pieces of retrieved context to answer the question. "
+                    "If you don't know the answer, just say that you don't know. don't try to make up an answer"
+                    "try to use exact wording from context that is relevant, Keep the answer concise but give details"
+                    "\nQuestion: {question} \nContext: {context} "
+                )
         return prompt
 
     def qa_chain(self):
